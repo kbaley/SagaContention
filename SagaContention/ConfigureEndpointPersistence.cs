eb33e553e9ffabdbf570b95b9afdb8e524bbb2fc
Kyle Baley <kyle@baley.org>
2020-04-15 17:11:35 -0500

    First commit

---
+++ SagaContention/ConfigureEndpointPersistence.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.AcceptanceTesting.Support;
+using NServiceBus.Persistence.Sql;
+using System.Data.SqlClient;
+
+class ConfigureEndpointPersistence : IConfigureEndpointTestExecution
+{
+    public Task Cleanup() {
+        return Task.CompletedTask;
+    }
+
+    public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings, PublisherMetadata publisherMetadata) {
+        var containerConnectionString = @"Data Source=localhost;Initial Catalog=nsb;Integrated Security=True";
+
+        var persistence = configuration.UsePersistence<SqlPersistence>();
+        persistence.SqlDialect<SqlDialect.MsSqlServer>();
+        persistence.ConnectionBuilder(
+            connectionBuilder: () => new SqlConnection(containerConnectionString));
+
+
+        return Task.FromResult(0);
+    }
+}
