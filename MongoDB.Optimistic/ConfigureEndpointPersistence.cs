18d25eb3c77610cbee18f3744b286b8b3dbfbd49
Kyle Baley <kyle@baley.org>
2020-04-27 16:33:08 -0500

    Add MongoDB

---
+++ MongoDB.Optimistic/ConfigureEndpointPersistence.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.AcceptanceTesting.Support;
+using MongoDB.Driver;
+
+class ConfigureEndpointPersistence : IConfigureEndpointTestExecution
+{
+    private const string databaseName = "AcceptanceTests";
+    private IMongoClient client;
+    public async Task Cleanup()
+    {
+        try {
+            await client.DropDatabaseAsync(databaseName);
+        }
+        // ReSharper disable once EmptyGeneralCatchClause
+        catch (Exception) { }
+    }
+
+    public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings, PublisherMetadata publisherMetadata)
+    {
+        var containerConnectionString = Environment.GetEnvironmentVariable("NServiceBusStorageMongoDB_ConnectionString");
+
+        client = string.IsNullOrWhiteSpace(containerConnectionString) ? new MongoClient() : new MongoClient(containerConnectionString);
+
+        configuration.UsePersistence<MongoPersistence>()
+            .MongoClient(client)
+            .DatabaseName(databaseName)
+            .UseTransactions(false);
+
+        return Task.FromResult(0);
+    }
+}
